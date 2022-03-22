9461da606d070674ba131ca858d3daba4d2cd80c
FatihG34 <fthgnydn34@gmail.com>
2022-03-22 17:37:47 +0300

    001_Assignment_011_01_Calculate_Profit.py was added to Assignments-Phyton

---
+++ 001_Assignment_011_01_Calculate_Profit.py
@@ -0,0 +1,4 @@
+depositd_mny = 1000  # main money
+daily_earn = depositd_mny * 0.07  #  brought 7% fixed profit daily
+ttl_weakly_earn = depositd_mny * 1.07**7  # $ 1000 reach at the end of the 7th day?
+print(ttl_weakly_earn)
