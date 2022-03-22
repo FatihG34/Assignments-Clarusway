bde92f367e515af46fbf87b13889396fbd649351
FatihG34 <fthgnydn34@gmail.com>
2022-03-22 17:44:49 +0300

    002_Assignment_011_02_Measure_Converter.py was added to Assignments-Phyton

---
+++ 002_Assignment_011_02_Measure_Converter.py
@@ -0,0 +1,3 @@
+temp1 = float(input("Please enter room temperature in degree of Celcius : "))
+temp2 = temp1*9/5 + 32  # to convert celsius to fahrenheit
+print("Your room temperature is", temp2, "°F")
