67742f0238fa1a6e4cd80bb0995ecad9f38a3fe6
FatihG34 <fthgnydn34@gmail.com>
2022-07-15 20:05:38 +0300

    new Assignment added

---
+++ 020_Assignment_Teamwork.py
@@ -0,0 +1,24 @@
+def print_max(arr, k):
+    my_list = []
+    max = 0
+    i = 0
+    for i in range(len(arr)-k+1):
+        max = arr[i]
+        i += 1
+        j = 1
+        while (j < k):
+            if arr[i+j] > max:
+                max = arr[i+j]
+                j += 1
+            my_list.append(max)
+            break
+    print(my_list)
+
+
+new_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+k = 3
+print_max(new_list, k)
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+l = 3
+print_max(nums, l)
